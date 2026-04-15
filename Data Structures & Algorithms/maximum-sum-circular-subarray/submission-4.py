5a9645f40d4c7821480e984553916c3897f14d80
Aakash Agarwal <aakash.agarwal.002@gmail.com>
2026-04-15 19:07:04 +0530

    Add: maximum-sum-circular-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def kadane(self, nums) -> int:
+        curr = 0
+        res = max(nums)
+        for i in range(len(nums)):
+            curr += nums[i]
+            if res < curr:
+                res = curr
+            if curr < 0:
+                curr = 0
+        print(res)
+        return res
+
+
+
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        total = sum(nums)
+        print(total)
+        neg_nums = [-i for i in nums]
+        max_kadane = self.kadane(nums)
+        max_neg_kadane = total + self.kadane(neg_nums)
+        if max_neg_kadane ==0:
+            return max_kadane
+        return max(max_kadane,max_neg_kadane)
+
